9ef463ac1f2ec1d628627f0b0620f2476ceee792
pbejaranob-sys <p.bejarano.b@outlook.com>
2026-05-06 15:06:52 -0500

    fix app export

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,14 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins: [react()], build: { commonjsOptions: { transformMixedEsModules: true } } });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  },
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore']
+  }
+})
